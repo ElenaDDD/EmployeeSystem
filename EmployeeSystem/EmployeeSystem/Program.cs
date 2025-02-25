380797146fbfab64bab4da5b36af3570042754cb
Olena dOBROMYSLOVA <dobromyselena@gmail.com>
2025-02-25 21:10:40 +0100

    feat: added AppDbContext

--- EmployeeSystem/EmployeeSystem/Program.cs
+++ EmployeeSystem/EmployeeSystem/Program.cs
@@ -1,4 +1,13 @@
+
+
+using EmployeeSystem.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseInMemoryDatabase("EmployeeDb")
+);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
