47fac2ac1ae5b00461d892c42a39e3d41441ca61
Olena dOBROMYSLOVA <dobromyselena@gmail.com>
2025-02-26 11:27:36 +0100

    feat: added Employee Repository interface

---
+++ EmployeeSystem/EmployeeSystem/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeSystem.Models;
+namespace EmployeeSystem.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> GetAllAsync();
+        Task <IEnumerable<Employee>> GetByIdAsync(int id);
+        Task AddEmployeeAsync(Employee employee);
+        Task UpdateEmployeeAsync(Employee employee);
+        Task DeleteEmployeeAsync(Employee employee);
+
+
+    }
+}
