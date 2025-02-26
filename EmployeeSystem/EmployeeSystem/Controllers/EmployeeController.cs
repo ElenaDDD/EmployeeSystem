4bb2ddcb6d15e4118c444181976b9abd1289fa14
Olena dOBROMYSLOVA <dobromyselena@gmail.com>
2025-02-26 18:07:51 +0100

    freat: added swagger

---
+++ EmployeeSystem/EmployeeSystem/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeSystem.Models;
+using EmployeeSystem.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+
+namespace EmployeeSystem.Controllers
+{
+    [Route("api/[Controller]")]
+
+    [ApiController]
+    public class EmployeeController: ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository= employeeRepository;
+        }
+        [HttpPost] 
+        public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
+        {
+            await _employeeRepository.AddEmployeeAsync(employee);
+            return Created();
+        }
+    }
+}
--- EmployeeSystem/EmployeeSystem/Program.cs
+++ EmployeeSystem/EmployeeSystem/Program.cs
@@ -1,7 +1,11 @@
 
 
 using EmployeeSystem.Data;
+using EmployeeSystem.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.OpenApi.Models;
+
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(
@@ -16,8 +20,27 @@ builder.Services.AddCors(opions =>
     })
 );
 
+//add the Emploee repository for DI
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1");
+        c.RoutePrefix = string.Empty;
+    });
+
+}
+app.MapControllers();
+
+app.UseCors("MyCors");
 
 app.Run();
