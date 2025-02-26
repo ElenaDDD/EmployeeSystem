24660c2b0e8c9a51a07c7e23f180c8167910dc33
Olena dOBROMYSLOVA <dobromyselena@gmail.com>
2025-02-26 11:14:51 +0100

    feat: configured Cors

--- EmployeeSystem/EmployeeSystem/Program.cs
+++ EmployeeSystem/EmployeeSystem/Program.cs
@@ -7,6 +7,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(
     options => options.UseInMemoryDatabase("EmployeeDb")
 );
+builder.Services.AddCors(opions =>
+   opions.AddPolicy("MyCors", builder =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    })
+);
 
 var app = builder.Build();
 
