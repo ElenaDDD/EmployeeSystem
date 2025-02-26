4ccfcacefb4ca3bc4bcab88896d741e84bd0e4ec
Olena dOBROMYSLOVA <dobromyselena@gmail.com>
2025-02-26 11:05:09 +0100

    feat: added Employee Model to dbContext

--- EmployeeSystem/EmployeeSystem/Data/AppDbContext.cs
+++ EmployeeSystem/EmployeeSystem/Data/AppDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using EmployeeSystem.Models;
 
 namespace EmployeeSystem.Data
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<Employee> Employees { get; set; }
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
             
