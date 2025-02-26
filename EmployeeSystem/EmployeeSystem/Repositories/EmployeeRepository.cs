70a73be4675c34fee2d77e40869944e7180c580f
Olena dOBROMYSLOVA <dobromyselena@gmail.com>
2025-02-26 16:52:42 +0100

    feat: added repository functionality

---
+++ EmployeeSystem/EmployeeSystem/Repositories/EmployeeRepository.cs
@@ -0,0 +1,48 @@
+﻿using EmployeeSystem.Data;
+using EmployeeSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace EmployeeSystem.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+       private readonly AppDbContext _context;
+        public EmployeeRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddEmployeeAsync(Employee employee)
+        {
+            await _context.Employees.AddAsync(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteEmployeeAsync(Employee employee)
+        {
+            var employeeEntity = await _context.Employees.FindAsync(employee.Id);
+            if (employeeEntity == null)
+            {
+                throw new KeyNotFoundException($"Employee with id : {employee.Id} was not found");
+            }
+            _context.Employees.Remove(employee); // Remove method is synchronous
+            await _context.SaveChangesAsync(); // Save changes asynchronously
+        }
+
+        public async Task<IEnumerable<Employee>> GetAllAsync()
+        {
+           return await _context.Employees.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Employee?>> GetByIdAsync(int id)
+        {
+            return (IEnumerable<Employee>)await _context.Employees.FindAsync(id);
+        }
+
+        public async Task UpdateEmployeeAsync(Employee employee)
+        {
+            _context.Employees.Update(employee); // Update method is synchronous
+            await _context.SaveChangesAsync();
+        }
+    }
+}
--- EmployeeSystem/EmployeeSystem/Repositories/IEmployeeRepository.cs
+++ EmployeeSystem/EmployeeSystem/Repositories/IEmployeeRepository.cs
@@ -4,7 +4,7 @@ namespace EmployeeSystem.Repositories
     public interface IEmployeeRepository
     {
         Task<IEnumerable<Employee>> GetAllAsync();
-        Task <IEnumerable<Employee>> GetByIdAsync(int id);
+        Task <IEnumerable<Employee?>> GetByIdAsync(int id);
         Task AddEmployeeAsync(Employee employee);
         Task UpdateEmployeeAsync(Employee employee);
         Task DeleteEmployeeAsync(Employee employee);
